fbff4b630834e7b4c46a5965a29615e5e41ce955
Ty Meador <disturbed.meador697@gmail.com>
2018-12-19 21:46:09 -0600

    Update documentation

--- documentation/typedefs.js
+++ documentation/typedefs.js
@@ -1,6 +1,6 @@
 /**
  * @typedef { Object } ApiAuth - An object containing your api credentials
- *   @property { string } key - The api key you created at https://www.bungie.net/en/Application
- *   @property { (number|string) } clientId - The oAuth client ID of your application. can be found at https://www.bungie.net/en/Application
- *   @property { string } userAgent - The user-agent you want to send with your api calls. The suggested format is “AppName/Version AppId/appIdNum (+webUrl;contactEmail)”
+ *   @property { string } ApiAuth.key - The api key you created at https://www.bungie.net/en/Application
+ *   @property { (number|string) } ApiAuth.clientId - The oAuth client ID of your application. can be found at https://www.bungie.net/en/Application
+ *   @property { string } ApiAuth.userAgent - The user-agent you want to send with your api calls. The suggested format is “AppName/Version AppId/appIdNum (+webUrl;contactEmail)”
  */
